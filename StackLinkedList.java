05670e15edf1e4d22267cf05d0218098cec30b81
beydaeren1 <beydaeren@posta.mu.edu.tr>
2025-05-23 12:03:09 +0300

    Add files via upload

---
+++ StackLinkedList.java
@@ -0,0 +1,30 @@
+public class StackLinkedList implements Stack{
+
+    StackItem top;
+    @Override
+    public void push(Object item) {
+        StackItem newBox = new StackItem(item);
+        StackItem previousTop = top;
+        top = newBox;
+        top.setNext(previousTop);
+    }
+    @Override
+    public Object pop() throws MyEmptyStackException{
+        if(this.top==null){
+            throw new MyEmptyStackException("Stack is empty you cant pop anything from empty stack")
+        }
+        Object tempData = top.getData();
+        top = top.getNext();
+        return tempData;
+    }
+
+    @Override
+    public void peek() {
+        System.out.println("The top is "
+        + this.top.getData());
+    }
+    @Override
+    public boolean isEmpty() {
+        return this.top == null;
+    }
+}
